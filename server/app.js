954565f86a2d2072bc30e400df8c00796625ad8f
Akarsh Pahariya <pahariyaakarsh@gmail.com>
2025-04-11 12:39:15 +0530

    security implementations

--- server/app.js
+++ server/app.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/userRouter');
 const groupRouter = require('./routes/groupRouter');
 const invitationRouter = require('./routes/invitationRouter');
 const globalErrorHandler = require('./controllers/errorController');
-const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again after an hour.',
+});
+app.use('/api', limiter);
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
-
 app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
